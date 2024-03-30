f0137ba0a46c4e51392f8a8ca3a4a2842a2e5019
Amrani-Farouk-Hossam-Eddine <119071864+Amrani-Farouk-Hossam-Eddine@users.noreply.github.com>
2024-03-30 01:51:50 +0100

    updating files

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -165,15 +165,13 @@ const Portfolio = () => {
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
           {portfolios.map(({ id, src, style, href, hrefCode, demo, alt }) => (
             <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
-              <LazyLoad height={200} offset={100} once>
-                <img
-                  loading='lazy'
-                  srcSet={`${src}?width=100 100w,${src}?width=200 200w,${src}?width=400 100w,${src}?width=100 400w,${src}?width=800 800w`}
-                  src={src}
-                  alt={alt}
-                  className={`rounded-md duration-200 hover:scale-105 ${style}`}
-                />
-              </LazyLoad>
+              <img
+                srcSet={`${src}?width=100 100w,${src}?width=200 200w,${src}?width=400 100w,${src}?width=100 400w,${src}?width=800 800w`}
+                src={src}
+                alt={alt}
+                className={`rounded-md duration-200 hover:scale-105 ${style}`}
+              />
+
               <div className='flex items-center justify-center'>
                 {demo && (
                   <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>
